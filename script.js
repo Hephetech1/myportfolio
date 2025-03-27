bf80ad7e7e009ec59418cc45196fcf700e7082e5
Hephetech1 <hephetech@gmail.com>
2025-03-27 05:14:18 +0100

    My first portfolio commit

---
+++ script.js
@@ -0,0 +1,37 @@
+  AOS.init();
+
+
+
+
+
+
+
+
+
+  
+
+window.addEventListener("scroll", function () {
+        var home= document.getElementById("home");
+        var navbar= 
+document.getElementById("navbar");
+if (window.scrollY > 50) {
+  navbar.classList.add("bg-dark");
+  home.classList.add("mt-4");
+  navbar.classList.remove("bg-light");
+} else {
+  navbar.classList.remove("bg-dark", "text-white");
+
+}
+
+      });
+     
+
+
+          const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navLinks.forEach(l => l.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
